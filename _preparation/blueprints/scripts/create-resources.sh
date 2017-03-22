f989c18e6e3dafe2929adf28032339bf17d3b8fa
Isaac Shabtay <isaac@shabtay.com>
2017-03-22 08:24:38 -0700

    3.4.2 adjustments

--- _preparation/blueprints/scripts/create-resources.sh
+++ _preparation/blueprints/scripts/create-resources.sh
@@ -5,4 +5,5 @@
 ctx logger info "Packaging resources from ${resources_root} into ${resources_target}..."
 
 mkdir -p ${resources_target}
-tar -cvzf ${resources_target}/static-app.tar.gz --directory ${resources_root}/static-app .
+cd ${resources_root}/static-app && zip -r ${resources_target}/static-app.zip .
+
