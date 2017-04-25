e361f4d7f2a151c20a7b9df474ef165726a601b2
Isaac Shabtay <isaac@shabtay.com>
2017-04-24 23:42:43 -0700

    4.0 adjustments

--- _preparation/blueprints/scripts/refresh-labs.sh
+++ _preparation/blueprints/scripts/refresh-labs.sh
@@ -2,8 +2,8 @@
 # We download the file and then push it, rather than downloading the file directly from the CLI machine.
 # Otherwise, credentials may be stored in the bash history on the CLI machine.
 SSH_PARMS=(-i ${key_filename} -o "StrictHostKeyChecking no")
-TEMP_LABS_ARCHIVE=$(mktemp -d --tmpdir labs.XXXXX)
-TEMP_NODECELLAR_ARCHIVE=$(mktemp -d --tmpdir nodecellar.XXXXX)
+TEMP_LABS_ARCHIVE=$(mktemp --tmpdir labs.XXXXX)
+TEMP_NODECELLAR_ARCHIVE=$(mktemp --tmpdir nodecellar.XXXXX)
 
 ctx logger info "Downloading labs archive: ${labs_archive} -> ${TEMP_LABS_ARCHIVE}"
 curl -L --user ${github_user}:${github_api_key} --output ${TEMP_LABS_ARCHIVE} ${labs_archive}
