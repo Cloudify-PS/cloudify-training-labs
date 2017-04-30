8814726d49833dceadda87c5d8b0d467a658fd5c
Isaac Shabtay <isaac@shabtay.com>
2017-04-29 19:50:21 -0700

    4.0 adjustments

--- _preparation/blueprints/scripts/install-cli.sh
+++ _preparation/blueprints/scripts/install-cli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-TMP_CLI=$(mktemp --tmpdir cli.XXXXX)
-curl -J -o $TMP_CLI ${cli_rpm}
-sudo yum install -y $TMP_CLI
-rm -rf $TMP_CLI
+SSH_PARMS=(-i ${key_filename} -o "StrictHostKeyChecking no")
+
+ssh "${SSH_PARMS[@]}" ${user}@${host} "curl -J -o /tmp/cfy-cli.rpm ${cli_rpm}"
+ssh "${SSH_PARMS[@]}" ${user}@${host} "sudo yum -y install /tmp/cfy-cli.rpm"
+ssh "${SSH_PARMS[@]}" ${user}@${host} "rm -rf /tmp/cfy-cli.rpm"
